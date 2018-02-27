e185f09f4a6ddcd5c3bedc959f4f9e89ddf3cf58
Ed Nykaza <edward.t.nykaza@gmail.com>
2018-02-27 11:28:08 -0600

    initial commit of get donor json files & mod env variables

--- environmentalVariables.py
+++ environmentalVariables.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-description: load environment variables (secrets into the workspace)
+description: load environment variables
 version: 0.0.1
 created: 2018-02-21
 author: Ed Nykaza
+dependencies:
+    * .env file in same folder as this script (Tidepool see bigdata 1PWD)
 license: BSD-2-Clause
 """
 
-# settings.py
+# %% load in required libraries
+import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+# %% load environmental variables
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+# %% define functions
+
+
+def get_environmental_variables(donorGroup):
+    envEmailVariableName = "BIGDATA_" + donorGroup + "_EMAIL"
+    emailAddress = os.environ[envEmailVariableName]
+
+    envPasswordVariableName = "BIGDATA_" + donorGroup + "_PASSWORD"
+    pswd = os.environ[envPasswordVariableName]
+
+    return emailAddress, pswd
