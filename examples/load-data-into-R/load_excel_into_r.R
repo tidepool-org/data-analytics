5ea6216987bf87b759cfefcb11dca813e0eff5d6
Unknown <kabolduc@maine.rr.com>
2018-07-17 19:06:10 -0400

    Added Header and other revisions.

--- examples/load-data-into-R/load_excel_into_r.R
+++ examples/load-data-into-R/load_excel_into_r.R
@@ -1,8 +1,24 @@
+############################################################################
+# description: Load donor data in Microsoft xls format into r data frame by 
+#              invidual sheet or as a merged data frame.
+# version: 0.0.1
+# created: 2018-07-2018
+# author: Kyle Bolduc
+# dependencies:
+#     * requires tidepool analysis environment (see readme for instructions)
+#     * readxl
+#     * readr
+#     * dplyr
+# 
+# license: BSD-2-Clause
+############################################################################
+
+
 ###Example of how to load a Microsoft Excel file from Tidepool into R:
 
 #Step 1, you need R package: readxl.  If it is not already installed in your system, then install it:
 
-if(require("readxl")==FALSE){install.packages("readxl")}
+if (require("readxl") == FALSE) {install.packages("readxl")}
 
 #Then load it:
 
@@ -10,12 +26,12 @@ library(readxl)
 
 #Step 2, identify the pathway to the file:
 
-pathway<-"../example-data/example-from-j-jellyfish.xlsx"
+pathway = file.path("..", "..", "example-data", "example-from-j-jellyfish.xlsx")
 
 
 #Step 3: Use the read_excel function to save the file as an object with any name you choose:
 
-tidepoolxl<-read_excel(pathway,sheet=1)
+tidepoolxl = read_excel(pathway, sheet = 1)
 
 #You need to load one sheet at a time into R.  The example Tidepool .xls file has 8 total sheets.  So repeat the above with sheet = 2 through 8
 # and rename the object each time if you want all sheets.
@@ -41,16 +57,16 @@ head(tidepoolxl, 10)
 
 #To save the file as a .csv file:
 #Set outpath:
-outpath<-"../example-data/example-from-j-jellyfish2.csv"
+outpath = file.path("..", "..", "example-data", "example-from-j-jellyfish2.csv")
 
-write.csv(tidepoolxl, file=outpath)
+write.csv(tidepoolxl, file = outpath)
 
 
 #To combine all Excel sheets into a single dataframe:
 
 
 #Install the readr package.
-if(require("readr")==FALSE){install.packages("readr")}
+if (require("readr") == FALSE) {install.packages("readr")}
 library(readr)
 
 
@@ -58,16 +74,14 @@ library(readr)
 xl_sheet_import <- function(path=pathway, n=1){
   
   #Import the data into an unnamed dataframe:
-  df<-
-    read_excel(path, sheet=n, col_names = FALSE)[-1,]
+  df = read_excel(path, sheet = n, col_names = FALSE)[-1,]
   
   #Collect the column names (first row of the sheet):
-  namesxl<-
-    read_excel(path, sheet=n, col_names = FALSE)[1,]
+  namesxl = read_excel(path, sheet = n, col_names = FALSE)[1,]
   
   #Apply the names to the dataframe:
 
-  colnames(df)<-namesxl
+  colnames(df) = namesxl
 
   return(df)
 }
@@ -76,22 +90,23 @@ xl_sheet_import <- function(path=pathway, n=1){
 
 merge_sheets <- function(path=pathway, n=length(excel_sheets(pathway))){
 
-    if(n<=1){
+    if (n <= 1) {
       return(xl_sheet_import(path))
     }else{
-      return(full_join(merge_sheets(path, n=n-1), xl_sheet_import(path, n)))
+      return(full_join(merge_sheets(path, n = n - 1), xl_sheet_import(path, n)))
     }
   
 }
 
 #Finally, use the readr function, type_convert(), to detect the data types of each column:
-combinedxl<-
-  readr::type_convert(merge_sheets(path=pathway))
+combinedxl = readr::type_convert(merge_sheets(path = pathway))
 
 
 #Inspect the results:
 str(combinedxl)
+
 dim(combinedxl)
+
 View(combinedxl)
 
 
--- examples/load-data-into-R/load_json_into_r.R
+++ examples/load-data-into-R/load_json_into_r.R
@@ -1,8 +1,20 @@
+############################################################################
+# description: Load donor data in json format into r data frame
+# version: 0.0.1
+# created: 2018-07-2018
+# author: Kyle Bolduc
+# dependencies:
+#     * requires tidepool analysis environment (see readme for instructions)
+#     * jsonlite
+# 
+# license: BSD-2-Clause
+############################################################################
+
 ###Example of how to load a .json file from Tidepool into R:
 
 #Step 1, you need R package: jsonlite.  If it is not already installed in your system, then install it:
 
-if(require("jsonlite")==FALSE){install.packages("jsonlite")}
+if (require("jsonlite") == FALSE) {install.packages("jsonlite")}
 
 #Then load it:
 
@@ -11,11 +23,11 @@ library("jsonlite")
 
 #Step 2, identify the pathway to the file:
 
-pathway<-"../example-data/example-from-j-jellyfish.json"
+pathway = file.path("..", "..", "example-data", "example-from-j-jellyfish.json")
 
 
 #Step 3: Use the fromJSON function to save the file as an object with any name you choose:
-tidepooljson<-fromJSON(pathway)
+tidepooljson = fromJSON(pathway)
 
 #Step 4: Now that the .json file is an R object, you can call commands on it:
 
@@ -33,8 +45,8 @@ head(tidepooljson, 10)
 
 #To save the data as a .csv file:
 #Set outpath:
-outpath<-"../example-data/example-from-j-jellyfish2.csv"
-write.csv(tidepooljson, file=outpath)
+outpath = file.path("..", "..", "example-data", "example-from-j-jellyfish2.json")
+write.csv(tidepooljson, file = outpath)
 
 
 
