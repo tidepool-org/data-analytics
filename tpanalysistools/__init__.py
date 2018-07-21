270587b9011f9f1a2b3e5f27b5beb4f484a7ce0d
Ed Nykaza <edward.t.nykaza@gmail.com>
2018-07-21 11:48:38 -0500

    initial commit

---
+++ tpanalysistools/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+description: initialize python package tidepool analysis tools package
+version: 0.0.1
+created: 2018-07-21
+author: Ed Nykaza
+license: BSD-2-Clause
+"""
