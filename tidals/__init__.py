912468f63672884305a9ef643ac1a6e26d0f3f12
Ed Nykaza <edward.t.nykaza@gmail.com>
2018-10-04 17:30:29 -0500

    update tidals init to include file path

--- tidals/__init__.py
+++ tidals/__init__.py
@@ -8,6 +8,16 @@ author: Ed Nykaza
 license: BSD-2-Clause
 """
 
+# add the tidals path to sys path if it doesn't already exist
+import os, sys
+dirname = os.path.dirname(os.path.realpath(__file__))
+if dirname not in sys.path:
+    sys.path.insert(0, dirname)
+
 # List of functions in the tidals package
 from load import load_json, load_csv, load_xlsx, load_data
-from clean import remove_duplicates, round_time, remove_brackets, flatten_json 
+from clean import remove_duplicates, round_time, remove_brackets, flatten_json
+
+# remove these modules and variables left over from the sys path
+# TODO: there has to be a better way to load in the path
+del os, sys, dirname
