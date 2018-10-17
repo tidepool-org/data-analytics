c654f9aee651fc6f20e98ede609211a708c85217
Ed Nykaza <edward.t.nykaza@gmail.com>
2018-10-17 07:09:09 -0500

    refactor init files and functions

--- examples/load-tidals-package.py
+++ examples/load-tidals-package.py
@@ -19,10 +19,10 @@ import tidals as td
 
 # %% load in example data with the tidals package
 dataPath = os.path.join(packagePath, "examples", "example-data", "example-from-j-jellyfish.csv")
-data = td.load_csv(dataPath)
+data = td.load.load_csv(dataPath)
 
 # get just the cgm data (utc-time and mmol/L values)
 cgm = data.loc[data.type == "cbg", ["time", "value"]]
 
 # round data to the nearest 15 minutes
-cgm = td.round_time(cgm, timeIntervalMinutes=15)
+cgm = td.clean.round_time(cgm, timeIntervalMinutes=15)
