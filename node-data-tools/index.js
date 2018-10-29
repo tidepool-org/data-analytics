97dc32727325368ec24cbf08b537933db9e07b1e
Lennart Goedhart <lennart@tidepool.org>
2018-10-29 19:43:43 +1100

    Initial commit of node-data-tools

---
+++ node-data-tools/index.js
@@ -0,0 +1,178 @@
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+import mkdirp from 'mkdirp';
+import events from 'events';
+import program from 'commander';
+import JSONStream from 'JSONStream';
+import es from 'event-stream';
+import flatten from 'flat';
+import Excel from 'exceljs';
+import * as CSV from 'csv-string';
+import config from './config.json';
+/* eslint no-console: ["error", { allow: ["warn", "error", "info"] }] */
+
+const allFields = _.chain(config)
+  .flatMap(field => Object.keys(field))
+  .uniq()
+  .sort()
+  .value();
+
+if (require.main === module) {
+  program
+    .version('0.1.0')
+    .option('-i, --input-tidepool-data <file>', 'csv, xlsx, or json file that contains Tidepool data')
+    .option('-c', '--config <file>', 'a JSON file that contains the field export configuration')
+    .option('--salt <salt>', 'salt used in the hashing algorithm', 'no salt specified')
+    .option('-o, --output-data-path <path>', 'the path where the data is exported',
+      path.join(__dirname, 'example-data', 'export'))
+    .option('-f, --output-format <format>', 'the path where the data is exported', (val, list) => {
+      list.push(val);
+      return list;
+    }, [])
+    /*
+    .option('--start-date [date]', 'filter data by startDate and EndDate')
+    .option('--end-date [date]', 'filter data by startDate and EndDate')
+    .option('--merge-wizard-data', 'option to merge wizard data with bolus data. Default is true')
+    .option(
+      '--filterByDatesExceptUploadsAndSettings',
+      'upload and settings data can occur before and after start and end dates, so include ALL upload and settings data in export',
+    )
+    */
+    .parse(process.argv);
+
+  if (!program.inputTidepoolData) program.help();
+  if (!program.outputFormat.length) {
+    program.outputFormat.push('all');
+  }
+
+  const inFilename = path.basename(program.inputTidepoolData, '.json');
+  const outFilename = path.join(program.outputDataPath,
+    crypto
+      .createHash('sha256')
+      .update(`${inFilename}${program.salt}`)
+      .digest('hex'));
+
+  const readStream = fs.createReadStream(program.inputTidepoolData);
+
+  readStream.on('error', () => {
+    console.error(`Could not read input file '${program.inputTidepoolData}'`);
+    process.exit(1);
+  });
+
+  readStream.on('open', () => {
+    if (!fs.existsSync(program.outputDataPath)) {
+      mkdirp.sync(program.outputDataPath, (err) => {
+        if (err) {
+          console.error(`Could not create export output path '${program.outputDataPath}'`);
+          process.exit(1);
+        }
+      });
+    }
+
+    let counter = 0;
+
+    // Data processing
+    const parser = JSONStream.parse('*');
+    const fieldsToStringify = _.flatMap(
+      config,
+      field => Object.keys(
+        _.pickBy(field, n => n.stringify),
+      ),
+    );
+
+    events.EventEmitter.defaultMaxListeners = 3;
+    const processingStream = readStream
+      .pipe(parser)
+      .pipe(es.mapSync((data) => {
+        // Stringify objects configured with { "stringify": true }
+        _.each(
+          _.chain(data)
+            .pick(fieldsToStringify)
+            .keys()
+            .value(),
+          item => _.set(data, item, JSON.stringify(data[item])),
+        );
+        return data;
+      }))
+      .pipe(es.mapSync(data => _.pick(flatten(data, {
+        // Map to flattened layout
+        maxDepth: 2,
+      }), allFields)));
+
+    // Single CSV
+    if (_.includes(program.outputFormat, 'csv') || _.includes(program.outputFormat, 'all')) {
+      events.EventEmitter.defaultMaxListeners += 2;
+      const csvStream = fs.createWriteStream(`${outFilename}.csv`);
+      csvStream.write(CSV.stringify(allFields));
+      processingStream
+        .pipe(es.mapSync((data) => {
+          counter += 1;
+          return CSV.stringify(allFields.map(field => data[field] || ''));
+        }))
+        .pipe(csvStream);
+    }
+
+    // Multiple CSVs
+    if (_.includes(program.outputFormat, 'csvs') || _.includes(program.outputFormat, 'all')) {
+      if (!fs.existsSync(outFilename)) {
+        fs.mkdirSync(outFilename);
+      }
+      Object.keys(config).forEach((key) => {
+        const csvStream2 = fs.createWriteStream(`${outFilename}/${key}.csv`);
+        csvStream2.write(CSV.stringify(Object.keys(config[key])));
+        events.EventEmitter.defaultMaxListeners += 2;
+        processingStream
+          // eslint-disable-next-line consistent-return
+          .pipe(es.mapSync((data) => {
+            if (data.type === key) {
+              return CSV.stringify(Object.keys(config[key]).map(field => data[field] || ''));
+            }
+          }))
+          .pipe(csvStream2);
+      });
+    }
+
+    // XLSX
+    if (_.includes(program.outputFormat, 'xlsx') || _.includes(program.outputFormat, 'all')) {
+      const xlsxStream = fs.createWriteStream(`${outFilename}.xlsx`);
+      const options = {
+        stream: xlsxStream,
+        useStyles: true,
+        useSharedStrings: true,
+      };
+      const wb = new Excel.stream.xlsx.WorkbookWriter(options);
+
+      events.EventEmitter.defaultMaxListeners += 1;
+      processingStream
+        .pipe(es.mapSync((data) => {
+          if (data.type) {
+            let sheet = wb.getWorksheet(data.type);
+            if (_.isUndefined(sheet)) {
+              sheet = wb.addWorksheet(data.type);
+              try {
+                sheet.columns = Object.keys(config[data.type]).map(field => ({
+                  header: field,
+                  key: field,
+                }));
+              } catch (e) {
+                console.warn(`Warning: configuration ignores data type: '${data.type}'`);
+              }
+            }
+            sheet.addRow(data).commit();
+          } else {
+            console.warn(`Invalid data type specified: '${JSON.stringify(data)}'`);
+          }
+        }));
+
+      processingStream.on('end', async () => {
+        await wb.commit();
+      });
+    }
+
+    readStream.on('end', () => {
+      console.info(`Exported ${counter} records.`);
+    });
+  });
+}
---
+++ node-data-tools/test/exporter.test.js
@@ -0,0 +1,96 @@
+import _ from 'lodash';
+import program from 'commander';
+import fs from 'fs';
+import Excel from 'exceljs';
+import { unflatten } from 'flat';
+import { diffString } from 'json-diff';
+
+program
+  .version('0.1.0')
+  .option('-i, --input-data <file>', 'csv, xlsx, or json file that contains Tidepool data')
+  .option('-o, --output-data <file>', 'the file that was exported by the exporter')
+  .option('-v, --verbose', 'show differences between files')
+  .parse(process.argv);
+
+const sortedInputData = _.sortBy(
+  JSON.parse(fs.readFileSync(program.inputData, 'utf8')),
+  obj => obj.time + obj.type,
+);
+
+const outputData = [];
+let sortedOutputData = [];
+let diffCount = 0;
+const excludedFields = [
+  '_deduplicator',
+  '_dataState',
+  'createdUserId',
+  'deletedTime',
+  'deletedUserId',
+  'modifiedUserId',
+  '_state',
+  'state',
+  'client',
+  'dataSetType',
+  'guid',
+  'payload', // TODO: should this be excluded?
+  'annotations', // TODO: should this be excluded?
+  'clockDriftOffset',
+  'conversionOffset',
+  'timezoneOffset',
+];
+
+const wb = new Excel.Workbook();
+
+(async () => {
+  await wb.xlsx.readFile(program.outputData);
+  wb.eachSheet((worksheet) => {
+    const headings = _.drop(worksheet.getRow(1).values);
+    worksheet.eachRow((row, rowNumber) => {
+      // Skip the header
+      if (rowNumber > 1) {
+        let valueIdx = 1;
+        const data = unflatten(_.omitBy(_.reduce(headings, (object, key) => {
+          let cellValue = row.values[valueIdx];
+          try {
+            cellValue = JSON.parse(cellValue);
+            if (typeof cellValue !== 'object') {
+              // cellValue = row.values[valueIdx];
+            }
+          } catch (e) {
+            // Don't care.
+          }
+          // eslint-disable-next-line no-param-reassign
+          object[key] = cellValue;
+          valueIdx += 1;
+          return object;
+        }, {}), _.isUndefined));
+        outputData.push(data);
+      }
+    });
+
+    sortedOutputData = _.sortBy(outputData, obj => obj.time + obj.type);
+  });
+
+  if (sortedInputData.length !== sortedOutputData.length) {
+    console.log('Number of input and output records don\'t match!');
+    process.exit(1);
+  }
+
+  for (let i = 0; i < sortedInputData.length; i++) {
+    const diff = diffString(_.omit(sortedInputData[i], excludedFields), sortedOutputData[i]);
+    if (diff) {
+      diffCount += 1;
+      console.log(`'${sortedInputData[i].type}' record at ${sortedInputData[i].time} differs`);
+      if (program.verbose) {
+        console.log(diff);
+      }
+    }
+  }
+
+  console.log(`There were ${diffCount} differences between ${sortedInputData.length} records.`);
+  if (diffCount === 0) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
+})();
