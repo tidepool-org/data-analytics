c96921c2e741ff14a089c6b34cb830c969c3e6c9
rpwils <rpwils@monsanto.com>
2018-12-23 19:24:45 -0600

    updated setup

--- tidepool-analysis-tools/setup.py
+++ tidepool-analysis-tools/setup.py
@@ -21,21 +21,30 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tidalsex",
+    name="tidalsexample",
     version="0.0.1",
-    author="Russ Wilson",
-    author_email="rpwils98@gmail.com",
+    author="",
+    author_email="",
     description="Tidepool Data Analysis Tools",
-    long_description="Tidepool Data Analysis Tools",
+    long_description="Tidepool Data Analysis Tools long description",
     long_description_content_type="text/markdown",
     url="https://github.com/rpwils/data-analytics/blob/master/tidepool-analysis-tools",
     packages=setuptools.find_packages(),
+    download_url="https://github.com/rpwils/data-analytics/archive/v0.0.2.tar.gz",
     classifiers=[
-        "Development Status :: 3 - Alpha"
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD-2-Clause",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
+    install_requires=[
+          'numpy>=1.14.1',
+          'pandas>=0.23.1',
+      ],
 )
 
 
