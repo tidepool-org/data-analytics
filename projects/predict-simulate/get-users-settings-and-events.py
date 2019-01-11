c8db909b4f9eeb0cd8d7ba07c1010cff906dad17
Ed Nykaza <edward.t.nykaza@gmail.com>
2019-01-11 13:42:51 -0600

    add bolus events

--- projects/predict-simulate/get-users-settings-and-events.py
+++ projects/predict-simulate/get-users-settings-and-events.py
@@ -222,6 +222,123 @@ def mmolL_to_mgdL(mmolL):
     return mmolL * 18.01559
 
 
+def round_time(df, timeIntervalMinutes=5, timeField="time",
+               roundedTimeFieldName="roundedTime", startWithFirstRecord=True,
+               verbose=False):
+    '''
+    A general purpose round time function that rounds the "time"
+    field to nearest <timeIntervalMinutes> minutes
+    INPUTS:
+        * a dataframe (df) that contains a time field that you want to round
+        * timeIntervalMinutes (defaults to 5 minutes given that most cgms output every 5 minutes)
+        * timeField to round (defaults to the UTC time "time" field)
+        * roundedTimeFieldName is a user specified column name (defaults to roundedTime)
+        * startWithFirstRecord starts the rounding with the first record if True, and the last record if False (defaults to True)
+        * verbose specifies whether the extra columns used to make calculations are returned
+    '''
+
+    df.sort_values(by=timeField, ascending=startWithFirstRecord, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    # make sure the time field is in the right form
+    t = pd.to_datetime(df[timeField])
+
+    # calculate the time between consecutive records
+    t_shift = pd.to_datetime(df[timeField].shift(1))
+    df["timeBetweenRecords"] = \
+        round((t - t_shift).dt.days*(86400/(60 * timeIntervalMinutes)) +
+              (t - t_shift).dt.seconds/(60 * timeIntervalMinutes)) * timeIntervalMinutes
+
+    # separate the data into chunks if timeBetweenRecords is greater than
+    # 2 times the <timeIntervalMinutes> minutes so the rounding process starts over
+    largeGaps = list(df.query("abs(timeBetweenRecords) > " + str(timeIntervalMinutes * 2)).index)
+    largeGaps.insert(0, 0)
+    largeGaps.append(len(df))
+
+    for gIndex in range(0, len(largeGaps) - 1):
+        chunk = t[largeGaps[gIndex]:largeGaps[gIndex+1]]
+        firstRecordChunk = t[largeGaps[gIndex]]
+
+        # calculate the time difference between each time record and the first record
+        df.loc[largeGaps[gIndex]:largeGaps[gIndex+1], "minutesFromFirstRecord"] = \
+            (chunk - firstRecordChunk).dt.days*(86400/(60)) + (chunk - firstRecordChunk).dt.seconds/(60)
+
+        # then round to the nearest X Minutes
+        # NOTE: the ".000001" ensures that mulitples of 2:30 always rounds up.
+        df.loc[largeGaps[gIndex]:largeGaps[gIndex+1], "roundedMinutesFromFirstRecord"] = \
+            round((df.loc[largeGaps[gIndex]:largeGaps[gIndex+1],
+                          "minutesFromFirstRecord"] / timeIntervalMinutes) + 0.000001) * (timeIntervalMinutes)
+
+        roundedFirstRecord = (firstRecordChunk + pd.Timedelta("1microseconds")).round(str(timeIntervalMinutes) + "min")
+        df.loc[largeGaps[gIndex]:largeGaps[gIndex+1], roundedTimeFieldName] = \
+            roundedFirstRecord + \
+            pd.to_timedelta(df.loc[largeGaps[gIndex]:largeGaps[gIndex+1],
+                                   "roundedMinutesFromFirstRecord"], unit="m")
+
+    # sort by time and drop fieldsfields
+    df.sort_values(by=timeField, ascending=startWithFirstRecord, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    if verbose is False:
+        df.drop(columns=["timeBetweenRecords",
+                         "minutesFromFirstRecord",
+                         "roundedMinutesFromFirstRecord"], inplace=True)
+
+    return df
+
+
+def get_descriptive_stats(df, newName, dataSubType):
+
+    newDf = df[dataSubType].describe().add_suffix(newName)
+
+    newDf[("rangeOf" + newName)] = \
+        newDf[("max" + newName)] - \
+        newDf[("min" + newName)]
+
+    return newDf
+
+
+def get_bolusDaySummary(bolusData):
+
+    if "extended" not in bolusData:
+        bolusData["extended"] = 0
+
+    bolusByDay = bolusData.groupby(bolusData["day"])
+
+    # total bolus insulin for each day
+    bolusDaySummary = pd.DataFrame(bolusByDay.normal.sum())
+    bolusDaySummary = bolusDaySummary.rename(columns={"normal":"totalAmountOfNormalBolusInsulin"})
+
+    bolusDaySummary["totalAmountOfExtendedBolusInsulin"] = bolusByDay.extended.sum().fillna(0.0)
+    bolusDaySummary["totalAmountOfBolusInsulin"] = bolusDaySummary["totalAmountOfNormalBolusInsulin"].fillna(0.0) + \
+                                           bolusDaySummary["totalAmountOfExtendedBolusInsulin"].fillna(0.0)
+
+    # bolus range for normal boluses
+    normalBasalDF = get_descriptive_stats(bolusByDay, "NormalBolusAmountPerBolus", "normal")
+    bolusDaySummary = pd.concat([bolusDaySummary, normalBasalDF], axis = 1)
+
+    # total number of bolus types per day
+    bolusTypePerDay = bolusData.groupby(["day",
+                                         "subType"]).size().unstack()
+
+    bolusDaySummary["numberOfNormalBoluses"] = bolusTypePerDay["normal"].fillna(0)
+
+    if "square" not in list(bolusTypePerDay):
+        bolusDaySummary["numberOfSquareBoluses"] = 0
+    else:
+        bolusDaySummary["numberOfSquareBoluses"] = bolusTypePerDay["square"].fillna(0)
+
+    if "dual/square" not in list(bolusTypePerDay):
+        bolusDaySummary["numberOfDualBoluses"] = 0
+    else:
+        bolusDaySummary["numberOfDualBoluses"] = bolusTypePerDay["dual/square"].fillna(0)
+
+    bolusDaySummary["numberOfAllBolusTypes"] = bolusDaySummary["numberOfNormalBoluses"] + \
+                                        bolusDaySummary["numberOfSquareBoluses"] + \
+                                        bolusDaySummary["numberOfDualBoluses"]
+
+    return bolusDaySummary
+
+
 # %% LOAD IN ONE FILE, BUT EVENTUALLY THIS WILL LOOOP THROUGH ALL USER'S
 dataPulledDate = "2018-09-28"
 phiDate = "PHI-" + dataPulledDate
@@ -284,11 +401,25 @@ if os.path.exists(jsonFileName):
             data["timezone"].fillna(method='bfill', inplace=True)
             data["day"] = pd.DatetimeIndex(data["utcTime"]).date
 
+            # round to the nearest 5 minutes
+            # TODO: once roundTime is pushed to tidals repository then this line can be replaced
+            # with td.clean.round_time
+            data = round_time(data, timeIntervalMinutes=5, timeField="time",
+                              roundedTimeFieldName="roundedTime", startWithFirstRecord=True,
+                              verbose=False)
+
+
 # %% ID, HASHID, AGE, & YLW
             data["userID"] = userID
             data["hashID"] = hashID
             data["age"] = np.floor((data["utcTime"] - bDate).dt.days/365.25).astype(int)
             data["ylw"] = np.floor((data["utcTime"] - dDate).dt.days/365.25).astype(int)
+
+            commonColumnHeadings = ["hashID",
+                                    "age",
+                                    "ylw",
+                                    "utcTime",
+                                    "roundedTime"]
 
 
 # %% FORMAT BOLUS DATA
@@ -301,17 +432,44 @@ if os.path.exists(jsonFileName):
 
 
 # %% ISF, CIR
+                # ISF
                 if "insulinSensitivities" in list(bolus):
                     pdb.set_trace()
 
-                # ISF
                 bolus["isf_mmolL_U"] = bolus["insulinSensitivity"]
                 bolus["isf"] = mmolL_to_mgdL(bolus["isf_mmolL_U"])
-                isf = bolus.loc[bolus["isf"].notnull(), ["utcTime", "isf", "isf_mmolL_U"]]
+
+                isfCH = commonColumnHeadings.copy()
+                isfCH.extend(["isf", "isf_mmolL_U"])
+                isf = bolus.loc[bolus["isf"].notnull(), isfCH]
 
                 # CIR
-                cir = bolus.loc[bolus["insulinCarbRatio"].notnull(), ["utcTime", "insulinCarbRatio"]]
+                if "carbRatios" in list(bolus):
+                    pdb.set_trace()
 
+                cirCH = commonColumnHeadings.copy()
+                cirCH.extend(["insulinCarbRatio"])
+                cir = bolus.loc[bolus["insulinCarbRatio"].notnull(), cirCH]
+
+
+# %% BOLUS EVENTS (CORRECTION, AND MEAL INCLUING: CARBS, EXTENDED, DUAL)
+                # get a summary of boluses per day
+                bolusDaySummary = get_bolusDaySummary(bolus)
+
+                if "extended" not in bolus:
+                    bolus["extended"] = np.nan
+                    bolus["duration"] = np.nan
+
+                bolusCH = commonColumnHeadings.copy()
+                bolusCH.extend(["normal", "carbInput", "subType",
+                                "insulinOnBoard", "bgInput"])
+                bolusEvents = bolus.loc[bolus["normal"].notnull(), bolusCH]
+                bolusEvents.loc[bolusEvents["bgInput"] == 0, "bgInput"] = np.nan
+                bolusEvents = bolusEvents.rename(columns={"normal": "unitsInsulin",
+                                                          "bgInput": "bg_mmolL"})
+                bolusEvents["bg_mgdL"] = mmolL_to_mgdL(bolusEvents["bg_mmolL"])
+                bolusEvents["eventType"] = "correction"
+                bolusEvents.loc[bolusEvents["carbInput"] == 0, "eventType"] = "meal"
 
 
 # %% INSULIN ACTIVITY DURATION
@@ -332,7 +490,7 @@ if os.path.exists(jsonFileName):
 # %% LOOP DATA (BINARY T/F)
 
 
-# %% BOLUS EVENTS (CORRECTION, AND MEAL INCLUING: CARBS, EXTENDED, DUAL)
+
 
 
 # %% CGM DATA
