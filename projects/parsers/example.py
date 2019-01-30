f06e26e22c39f0485742807226c24d40ad31c0d0
Ed Nykaza <edward.t.nykaza@gmail.com>
2019-01-30 10:54:00 -0600

    update example to export csv file

--- projects/parsers/example.py
+++ projects/parsers/example.py
@@ -7,6 +7,7 @@ Created on Fri Jan 25 06:55:48 2019
 """
 
 from loop_report import LoopReport
+import pandas as pd
 import json
 
 # %% file name and path
@@ -21,5 +22,16 @@ loop_dict = lr.parse_by_file(
 )
 
 # %% save parsed file
-with open(file_name + '-data.json', 'w') as fp:
+with open(file_path + file_name + '-data.json', 'w') as fp:
     json.dump(loop_dict, fp, sort_keys=True, indent=4)
+
+# %% put data into a dataframe and save
+loop_df = pd.DataFrame(columns=loop_dict.keys(), index=[0])
+loop_df = loop_df.astype('object')
+for k in loop_dict.keys():
+    loop_df[k][0] = loop_dict[k]
+
+loop_df.to_csv(file_path + file_name + '-data-in-columns.csv')
+loop_df.T.to_csv(file_path + file_name + '-data-in-rows.csv')
+
+
