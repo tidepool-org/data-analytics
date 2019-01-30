066904bf236e088294ce076ae582263961a3ec92
Ed Nykaza <edward.t.nykaza@gmail.com>
2019-01-30 11:37:02 -0600

    include index in output and the same file name for input and output

--- projects/parsers/example.py
+++ projects/parsers/example.py
@@ -12,13 +12,13 @@ import json
 
 # %% file name and path
 file_path = "/Users/ed/Desktop/"
-file_name = "2019-01-16 08-39-03-06-00"
+file_name = "Loop Report 2019-01-16 08-39-03-06-00"
 
 # %% parse file
 lr = LoopReport()
 loop_dict = lr.parse_by_file(
         path=file_path,
-        file_name="Loop Report %s.md" % file_name
+        file_name="%s.md" % file_name
 )
 
 # %% save parsed file
@@ -31,7 +31,5 @@ loop_df = loop_df.astype('object')
 for k in loop_dict.keys():
     loop_df[k][0] = loop_dict[k]
 
-loop_df.to_csv(file_path + file_name + '-data-in-columns.csv')
-loop_df.T.to_csv(file_path + file_name + '-data-in-rows.csv')
-
-
+loop_df.to_csv(file_path + file_name + '-data-in-columns.csv', index_label="index")
+loop_df.T.to_csv(file_path + file_name + '-data-in-rows.csv', index_label="index")
