a7ef0f6e32316b4c1ee9c5e2958d3dbb5b0b4a6f
Ed Nykaza <edward.t.nykaza@gmail.com>
2019-02-19 17:17:45 -0600

    expand parser_client to batch process md files in directory

--- projects/parsers/parser_client.py
+++ projects/parsers/parser_client.py
@@ -1,66 +1,132 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-description: Example code using the loop_report class.
-Created on Fri Jan 25 06:55:48 2019
 
-@author: ed
 
-commandline example to run the parser :
-python parser_client.py ../tests/parsers/files LoopReport.md
-"""
-
+# %% REQUIRED LIBRARIES
+import argparse
 from loop_report import LoopReport
 import pandas as pd
 import json
 import os
-import sys
+import datetime as dt
 
 
-def parse_by_file(file_path, file_name):
+# %% CODE DESCRIPTION
+codeDescription = (
+    "Parses Loop issue report(s) into a dictionary," +
+    "and saves the data to user specified format (json or csv)"
+)
 
-    output_path = os.path.join(".", "output")
+
+# %% FUNCTIONS
+def parse_by_file(file_path, file_name, output_path):
+
+    if not os.path.isfile(os.path.join(file_path, file_name)):
+        raise RuntimeError("The file name is invalid.")
+
     # make an output folder if it does not exist
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-
-    # %% parse file
+    # % parse file
     lr = LoopReport()
     loop_dict = lr.parse_by_file(path=file_path, file_name=file_name)
 
-
-    # %% put data into a dataframe and save
-    loop_df = pd.DataFrame(columns=loop_dict.keys(), index=[0])
-    loop_df = loop_df.astype("object")
-    for k in loop_dict.keys():
-        loop_df[k][0] = loop_dict[k]
-
-    loop_df.to_csv(
-        os.path.join(output_path, file_name + "-data-in-columns.csv"), index_label="index"
-    )
-    loop_df.T.to_csv(
-        os.path.join(output_path, file_name + "-data-in-rows.csv"), index_label="index"
+    # save a pretty json
+    output_path_name = os.path.join(
+        output_path, file_name[:-3] + "-parsed.json"
     )
 
-    output_path = os.path.join("./", "output/")
-    # %% if we remove the embedded data frames, we can save a pretty json
-    with open(output_path + file_name + "-data.json", "w") as fp:
+    with open(output_path_name, "w") as fp:
         json.dump(loop_dict, fp, sort_keys=True, indent=4)
 
-    print("file parsed")
+    print(file_name, "file parsed")
+
+    return loop_dict
 
 
-def main():
-    file_path = sys.argv[1]
-    file_name = sys.argv[2]
-    if not os.path.isdir(file_path):
+def parse_directory(file_path, output_path):
+    all_loop_df = pd.DataFrame()
+    for file in os.listdir(args.file_path):
+        if ".md" in file:
+            loop_dict = parse_by_file(file_path, file, output_path)
+
+            loop_df = pd.DataFrame(columns=loop_dict.keys(), index=[0])
+            loop_df = loop_df.astype("object")
+            for k in loop_dict.keys():
+                loop_df[k][0] = loop_dict[k]
+
+            all_loop_df = pd.concat(
+                [all_loop_df, loop_df],
+                sort=False,
+                ignore_index=True)
+
+            process_date = dt.datetime.now().strftime("%Y-%m-%d")
+            output_path_name = os.path.join(
+                output_path, process_date + "-batch-parsing.csv"
+            )
+
+            all_loop_df.to_csv(output_path_name, index_label="index")
+
+    return all_loop_df
+
+
+# %% COMMAND LINE ARGUMENTS
+def main(args):
+
+    if not os.path.isdir(args.file_path):
         raise RuntimeError("The file path is invalid.")
 
-    if not os.path.isfile(f"{file_path}/{file_name}"):
-        raise RuntimeError("The file name is invalid.")
+    if args.batch_process:  # process all md files in path
+        output = parse_directory(args.file_path, args.output_path)
 
-    parse_by_file(file_path, file_name)
+    else:  # process one file
+        output = parse_by_file(
+            args.file_path,
+            args.file_name,
+            args.output_path
+        )
+
+    return output
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description=codeDescription)
+    parser.add_argument(
+        "-p",
+        "--path",
+        dest="file_path",
+        default=os.path.join("..", "tests", "parsers", "files"),
+        help="directory of the issue report file(s)"
+    )
+    parser.add_argument(
+        "-n",
+        "--file_name",
+        dest="file_name",
+        default="LoopReport.md",
+        help="directory of the issue report file(s)"
+    )
+    parser.add_argument(
+        "-o",
+        "--output_path",
+        dest="output_path",
+        default=os.path.join(".", "output", ""),
+        help="directory of where to save the output file(s)"
+    )
+    parser.add_argument(
+        "-b",
+        "--batch",
+        dest="batch_process",
+        default=True,
+        help="True if you want to process all issue reports in the dir",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose_output",
+        default=True,
+        help="True if you want script progress to print to the console",
+    )
+    args = parser.parse_args()
+
+    output = main(args)
