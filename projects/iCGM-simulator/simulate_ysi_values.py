f3edd62632d197281abec38b3b70f653865e4d49
Ed Nykaza <edward.t.nykaza@gmail.com>
2019-06-14 07:44:12 -0500

    initial commit

---
+++ projects/iCGM-simulator/simulate_ysi_values.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# %% REQUIRED LIBRARIES
+import argparse
+
+
+# %% CODE DESCRIPTION
+codeDescription = (
+    "simulate ysi values from iCGM values using the reported distributions" +
+    "in Table 3A and 4A from the " +
+    "Dexcom G6 Continuous Glucose Monitoring System User Guide:"
+    "https://s3-us-west-2.amazonaws.com/dexcompdf/G6-CGM-Users-Guide.pdf"
+)
+
+
+# %% FUNCTIONS
+#def main_function():
+#    return
+
+
+# %% START OF CODE
+
+#
+
+# %% COMMAND LINE ARGUMENTS
+#def main(args):
+#    main_function(args)
+#
+#
+#if __name__ == "__main__":
+#    parser = argparse.ArgumentParser(description=codeDescription)
+##    parser.add_argument(
+##        "-<single letter>",
+##        "--<longer description>",
+##        dest="<vairable_name>",
+##        default=<default_variable_value>,
+##        help="<help descripition>"
+##    )
+#
+#    args = parser.parse_args()
+#
+#    main(args)
+
