99de5e567d951c40b5bf9767a42fb43e697f76cf
Jason Meno <jasonmeno@msn.com>
2020-03-04 11:51:44 -0500

    Add column names filled in function descriptions

--- projects/normalized-data-format/normalized-data-sample-generator.py
+++ projects/normalized-data-format/normalized-data-sample-generator.py
@@ -64,49 +64,84 @@ def add_time_data(
         sample_dataset,
         days_of_data
 ):
-    """Adds contiguously rounded time range to dataset"""
+    """Adds contiguously rounded time range to dataset columns:
+        * timeUTC
+        * timeLocal
+        * timeDevice
+    """
 
     return sample_dataset
 
 
 def add_bgValue_data(sample_dataset):
-    """Adds bgValue data to dataset"""
+    """Adds bgValue data to dataset columns:
+        * bgValueCGM
+        * bgValueSMBG
+        * bgValueCalibration
+        * bgValuePumpCalculator
+    """
 
     return sample_dataset
 
 
 def add_insulin_data(sample_dataset):
-    """Adds insulin data to dataset"""
+    """Adds insulin data to dataset to dataset columns:
+        * insulinDeliveredTotal
+        * insulinDeliveredBasal
+        * insulinDeliveredBolus
+        * insulinDeliveredExtended
+    """
 
     return sample_dataset
 
 
 def add_carb_data(sample_dataset):
-    """Adds carb data to dataset"""
+    """Adds carb data to dataset to dataset columns:
+        * carbIntake
+    """
 
     return sample_dataset
 
 
 def add_exercise_data(sample_dataset):
-    """Adds exercise data to dataset"""
+    """Adds exercise data to dataset to dataset columns:
+        * exerciseStatus
+        * exerciseName
+        * exerciseDuration
+        * exerciseDistance
+        * exerciseEnergy
+    """
 
     return sample_dataset
 
 
 def add_basal_data(sample_dataset):
-    """Adds basal rate data to dataset"""
+    """Adds basal rate data to dataset columns:
+        * basalRate
+        * basalDuration
+        * basalType
+    """
 
     return sample_dataset
 
 
 def add_alarm_data(sample_dataset):
-    """Adds alarm data to dataset"""
+    """Adds alarm data to dataset to dataset columns:
+        * alarmCGMStatus
+        * alarmCGMType
+        * alarmPumpStatus
+        * alarmPumpType
+    """
 
     return sample_dataset
 
 
 def add_settings_data(sample_dataset):
-    """Adds settings data to dataset"""
+    """Adds settings data to dataset columns:
+        * settingsPumpBGTarget
+        * settingsPumpInsulinSensitivity
+        * settingsPumpCarbRatio
+    """
 
     return sample_dataset
 
