f585522e580da21568cf645d317565286a6a00ea
Jason Meno <jasonmeno@msn.com>
2020-03-31 19:31:13 -0400

    Add data creation logic to settings, basal, carbs, and insulin

--- projects/normalized-data-format/normalized-data-sample-generator.py
+++ projects/normalized-data-format/normalized-data-sample-generator.py
@@ -18,7 +18,7 @@ used by the Tidepool Data Science Team
 
 import pandas as pd
 import numpy as np
-
+import datetime
 # %% Functions
 
 
@@ -26,33 +26,32 @@ def create_dataframe_template():
     """Creates a template of columns to be used by the data generator"""
 
     df_columns = [
-        'timeUTC'
-        'timeLocal'
-        'timeDevice'
-        'bgValueCGM'
-        'bgValueSMBG'
-        'bgValueCalibration'
-        'bgValuePumpCalculator'
-        'insulinDeliveredTotal'
-        'insulinDeliveredBasal'
-        'insulinDeliveredBolus'
-        'insulinDeliveredExtended'
-        'carbIntake'
-        'exerciseStatus'
-        'exerciseName'
-        'exerciseDuration'
-        'exerciseDistance'
-        'exerciseEnergy'
-        'basalRate'
-        'basalDuration'
-        'basalType'
-        'alarmCGMStatus'
-        'alarmCGMType'
-        'alarmPumpStatus'
-        'alarmPumpType'
-        'settingsPumpBGTarget'
-        'settingsPumpInsulinSensitivity'
-        'settingsPumpCarbRatio'
+        'timeUTC',
+        'timeLocal',
+        'bgValueCGM',
+        'bgValueSMBG',
+        'bgValueCalibration',
+        'bgValuePumpCalculator',
+        'insulinDeliveredTotal',
+        'insulinDeliveredBasal',
+        'insulinDeliveredBolus',
+        'insulinDeliveredExtended',
+        'carbIntake',
+        'exerciseStatus',
+        'exerciseName',
+        'exerciseDuration',
+        'exerciseDistance',
+        'exerciseEnergy',
+        'basalRate',
+        'basalDuration',
+        'basalType',
+        'alarmCGMStatus',
+        'alarmCGMType',
+        'alarmPumpStatus',
+        'alarmPumpType',
+        'settingsPumpBGTarget',
+        'settingsPumpInsulinSensitivity',
+        'settingsPumpCarbRatio',
     ]
 
     template_df = pd.DataFrame(columns=df_columns)
@@ -60,20 +59,26 @@ def create_dataframe_template():
     return template_df
 
 
-def add_time_data(
-        sample_dataset,
-        days_of_data
-):
+def add_time_data(sample_dataset, days_of_data):
     """Adds contiguously rounded time range to dataset columns:
         * timeUTC
         * timeLocal
         * timeDevice
     """
 
+    start_time = pd.to_datetime("2015-01-01", utc=True)
+    end_time = start_time + datetime.timedelta(days=days_of_data)
+
+    contiguous_ts = pd.date_range(start_time, end_time, freq="5min")
+    local_ts = contiguous_ts.tz_convert('US/Pacific')
+
+    sample_dataset['timeUTC'] = contiguous_ts.strftime('%Y-%m-%dT%H:%M:%S')
+    sample_dataset['timeLocal'] = local_ts.strftime('%Y-%m-%dT%H:%M:%S')
+
     return sample_dataset
 
 
-def add_bgValue_data(sample_dataset):
+def add_bgValue_data(sample_dataset, days_of_data):
     """Adds bgValue data to dataset columns:
         * bgValueCGM
         * bgValueSMBG
@@ -81,10 +86,42 @@ def add_bgValue_data(sample_dataset):
         * bgValuePumpCalculator
     """
 
+    sample_dataset['bgValueCGM'] = 100
+
+    smbg_samples = days_of_data*3
+    smbg_indices = np.random.choice(
+            sample_dataset.index,
+            smbg_samples,
+            replace=False
+    )
+    sample_dataset.loc[smbg_indices, 'bgValueSMBG'] = (
+        sample_dataset.loc[smbg_indices, 'bgValueCGM']
+    )
+
+    calib_samples = days_of_data
+    calib_indices = np.random.choice(
+            sample_dataset.index,
+            calib_samples,
+            replace=False
+    )
+    sample_dataset.loc[calib_indices, 'bgValueCalibration'] = (
+        sample_dataset.loc[calib_indices, 'bgValueCGM']
+    )
+
+    pump_samples = days_of_data*3
+    pump_indices = np.random.choice(
+            sample_dataset.index,
+            pump_samples,
+            replace=False
+    )
+    sample_dataset.loc[pump_indices, 'bgValuePumpCalculator'] = (
+        sample_dataset.loc[pump_indices, 'bgValueCGM']
+    )
+
     return sample_dataset
 
 
-def add_insulin_data(sample_dataset):
+def add_insulin_data(sample_dataset, days_of_data):
     """Adds insulin data to dataset to dataset columns:
         * insulinDeliveredTotal
         * insulinDeliveredBasal
@@ -92,18 +129,79 @@ def add_insulin_data(sample_dataset):
         * insulinDeliveredExtended
     """
 
+    # Add Correction Bolus Data
+    bolus_samples = days_of_data*2
+    boluses = np.random.uniform(1, 20, bolus_samples)
+    boluses = np.round(np.round(boluses/0.05)*0.05, 2)
+    bolus_indices = np.random.choice(
+            sample_dataset.index,
+            bolus_samples,
+            replace=False
+    )
+    sample_dataset.loc[bolus_indices, 'insulinDeliveredBolus'] = (
+        boluses
+    )
+
+    # Add Meal Bolus data from carbIntake
+    carb_indices = sample_dataset['carbIntake'].notnull()
+    cir = sample_dataset.loc[carb_indices, 'settingsPumpCarbRatio']
+    carbs = sample_dataset.loc[carb_indices, 'carbIntake']
+    meal_boluses = np.round(np.round((carbs/cir).astype(float)/0.05)*0.05, 2)
+    sample_dataset.loc[carb_indices, 'insulinDeliveredBolus'] = meal_boluses
+
+    # Add Extended Bolus Data
+    extended_samples = days_of_data//2
+    extended_boluses = np.random.uniform(0.1, 1.5, extended_samples).round(2)
+    extended_indices = np.random.choice(
+            bolus_indices,
+            extended_samples,
+            replace=False
+    )
+    sample_dataset.loc[extended_indices, 'insulinDeliveredExtended'] = (
+        extended_boluses
+    )
+    sample_dataset['insulinDeliveredExtended'].ffill(limit=23, inplace=True)
+    sample_dataset['insulinDeliveredExtended'] = (
+        round(round(sample_dataset['insulinDeliveredExtended']/0.05)*0.05, 2)
+    )
+
+    # Add Basal Data
+    sample_dataset['insulinDeliveredBasal'] = sample_dataset['basalRate']/12
+    sample_dataset['insulinDeliveredBasal'] = (
+            round(round(sample_dataset['insulinDeliveredBasal']/0.05)*0.05, 2)
+    )
+
+    # Calculate Total Insulin
+    sample_dataset['insulinDeliveredTotal'] = (
+            sample_dataset[
+                    ['insulinDeliveredBasal',
+                     'insulinDeliveredBolus',
+                     'insulinDeliveredExtended'
+                     ]
+            ].sum(axis=1)
+    )
+
     return sample_dataset
 
 
-def add_carb_data(sample_dataset):
+def add_carb_data(sample_dataset, days_of_data):
     """Adds carb data to dataset to dataset columns:
         * carbIntake
     """
 
+    carb_samples = days_of_data*3
+    carbs = np.random.randint(1, 120, carb_samples)
+    carb_indices = np.random.choice(
+            sample_dataset.index,
+            carb_samples,
+            replace=False
+    )
+    sample_dataset.loc[carb_indices, 'carbIntake'] = carbs
+
     return sample_dataset
 
 
-def add_exercise_data(sample_dataset):
+def add_exercise_data(sample_dataset, days_of_data):
     """Adds exercise data to dataset to dataset columns:
         * exerciseStatus
         * exerciseName
@@ -115,12 +213,48 @@ def add_exercise_data(sample_dataset):
     return sample_dataset
 
 
-def add_basal_data(sample_dataset):
+def add_basal_data(sample_dataset, days_of_data):
     """Adds basal rate data to dataset columns:
         * basalRate
         * basalDuration
         * basalType
     """
+
+    # Add basal rates
+    basal_num = np.random.randint(1, 12)
+    basal_rates = np.random.uniform(0.5, 3, basal_num).round(1)
+    start_times = np.sort(
+            np.random.choice(np.arange(24), basal_num, replace=False)
+    )
+    basal_map = dict(zip(start_times, basal_rates))
+
+    hourly_rates = pd.Series(np.arange(24)).map(basal_map).ffill()
+    hourly_rates.fillna(hourly_rates.values[-1], inplace=True)
+    hourly_map = dict(zip(hourly_rates.index, hourly_rates))
+
+    hourly_ts = pd.to_datetime(sample_dataset.timeLocal).dt.hour
+
+    basal_rate_ts = hourly_ts.map(hourly_map)
+    sample_dataset['basalRate'] = basal_rate_ts
+
+    # Add Basal Durations
+    basal_durations = 60 * np.diff(
+            np.concatenate(
+                    [start_times, [1 + start_times[-1] + start_times[0]]]
+            )
+    )
+
+    basal_duration_map = dict(zip(start_times, basal_durations))
+
+    hourly_duration = pd.Series(np.arange(24)).map(basal_duration_map).ffill()
+    hourly_duration.fillna(hourly_duration.values[-1], inplace=True)
+    hourly_duration_map = dict(zip(hourly_duration.index, hourly_duration))
+
+    basal_duration_ts = hourly_ts.map(hourly_duration_map)
+
+    sample_dataset['basalDuration'] = basal_duration_ts
+
+    sample_dataset['basalType'] = 'scheduled'
 
     return sample_dataset
 
@@ -143,15 +277,75 @@ def add_settings_data(sample_dataset):
         * settingsPumpCarbRatio
     """
 
+    settings_num = np.random.randint(1, 12)
+    bg_targets = np.random.uniform(70, 160, settings_num).round()
+    isfs = np.random.uniform(15, 60, settings_num).round()
+    cirs = np.random.uniform(5, 20, settings_num).round()
+
+    start_times = np.sort(
+            np.random.choice(np.arange(24), settings_num, replace=False)
+    )
+    bg_target_map = dict(zip(start_times, bg_targets))
+    isf_map = dict(zip(start_times, isfs))
+    cir_map = dict(zip(start_times, cirs))
+
+    hourly_targets = pd.Series(np.arange(24)).map(bg_target_map).ffill()
+    hourly_targets.fillna(hourly_targets.values[-1], inplace=True)
+    hourly_target_map = dict(zip(hourly_targets.index, hourly_targets))
+
+    hourly_isfs = pd.Series(np.arange(24)).map(isf_map).ffill()
+    hourly_isfs.fillna(hourly_isfs.values[-1], inplace=True)
+    hourly_isf_map = dict(zip(hourly_isfs.index, hourly_isfs))
+
+    hourly_cir = pd.Series(np.arange(24)).map(cir_map).ffill()
+    hourly_cir.fillna(hourly_cir.values[-1], inplace=True)
+    hourly_cir_map = dict(zip(hourly_cir.index, hourly_cir))
+
+    hourly_ts = pd.to_datetime(sample_dataset.timeLocal).dt.hour
+
+    bg_target_ts = hourly_ts.map(hourly_target_map)
+    isf_ts = hourly_ts.map(hourly_isf_map)
+    cir_ts = hourly_ts.map(hourly_cir_map)
+
+    sample_dataset['settingsPumpBGTarget'] = bg_target_ts
+    sample_dataset['settingsPumpInsulinSensitivity'] = isf_ts
+    sample_dataset['settingsPumpCarbRatio'] = cir_ts
+
     return sample_dataset
 
 
-def make_dataset():
+def make_dataset(days_of_data):
     """Main function wrapper to create a sample normalized dataset"""
+
+    # Start with empty dataset template
+    sample_dataset = create_dataframe_template()
+
+    # Create timeseries with a certain # days of data
+    sample_dataset = add_time_data(sample_dataset, days_of_data)
+
+    # Add settings and basal rates
+    sample_dataset = add_settings_data(sample_dataset)
+    sample_dataset = add_basal_data(sample_dataset, days_of_data)
+
+    # Add in carb entries
+    sample_dataset = add_carb_data(sample_dataset, days_of_data)
+
+    # Add insulin data based on carbs and settings
+    sample_dataset = add_insulin_data(sample_dataset, days_of_data)
+
+    # Add exercise data
+    sample_dataset = add_exercise_data(sample_dataset, days_of_data)
+
+    # Add blood glucose data
+    sample_dataset = add_bgValue_data(sample_dataset, days_of_data)
+
+    # Add alarms
+    sample_dataset = add_alarm_data(sample_dataset, days_of_data)
 
     return sample_dataset
 
 
 # %% Main function call
 if __name__ == "__main__":
-    sample_dataset = make_dataset()
+    days_of_data = 30
+    sample_dataset = make_dataset(days_of_data)
